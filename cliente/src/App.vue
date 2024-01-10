685dd670c992f14ec85ef4f92710e40412994b59
J. Miguel Aguario Diaz <i20213tn095@utez.edu.mx>
2024-01-09 19:41:19 -0600

    Initial commit and navbar

---
+++ cliente/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <div id="app">
+      <Navbar/>
+    <div>
+      
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
