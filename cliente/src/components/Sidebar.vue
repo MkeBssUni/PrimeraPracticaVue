c21767edb0d10c65e82f1deb4fb661925bf642cf
J. Miguel Aguario Diaz <i20213tn095@utez.edu.mx>
2024-01-12 16:07:01 -0600

    fixed navbar

--- cliente/src/components/Sidebar.vue
+++ cliente/src/components/Sidebar.vue
@@ -1,23 +1,26 @@
 <template>
-    <div>
-      <div class="bg-light border-right" id="sidebar-wrapper">
-        <div class="px-3 py-2">
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-          </p>
-          <b-img src="https://imgs.search.brave.com/Z6LzjxmQuRuliNsXrF4Y_weny0PA6aGBxfQRHKyO6RA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9sYXN0/Zm0uZnJlZXRscy5m/YXN0bHkubmV0L2kv/dS81MDB4NTAwL2E5/MzczOTk3NGE1MGE5/YTYzNDA5ZTIwYTA3/OWQxMDNmLmpwZw" fluid thumbnail></b-img>
-        </div>
-      </div>
-    </div>
+    <b-sidebar id="sidebar-1" shadow
+    z-index="-1"
+    visible
+    no-close-on-route-change
+    no-close-on-backdrop
+    no-header-close
+    >
+      <b-nav vertical class="pt-5">
+        <b-nav-item>
+          <b-link :to="{name:'inicio'}">Inicio</b-link>
+        </b-nav-item>
+        <b-nav-item>
+          <b-link :to="{name:'main'}">Main</b-link>
+        </b-nav-item>
+        <b-nav-item>
+          <b-link :to="{name:'tercero'}">Tercero</b-link>
+        </b-nav-item>
+      </b-nav>
+    </b-sidebar>
   </template>
   
   <style scoped>
-  #sidebar-wrapper {
-    min-height: 100vh;
-    width: 20rem; 
-    background-color: black;
-    box-shadow: 0 0 10px rgba(0,0,0,0.5);
-  }
+
   </style>
   
